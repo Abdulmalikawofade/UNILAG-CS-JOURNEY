6b3848d4f4da910842e7f8df5662e86aec3ae27f
Abdulmalikawofade <abdulmalikawofade@gmail.com>
2026-05-01 15:35:07 -0700

    Day 2: Varisbles + f-strings. 1 year to UNILAG for Abdulmalik

---
+++ day2.py
@@ -0,0 +1,12 @@
+#Day 2: Variable - making python remember
+name = "Abdulmalik"
+school = "UNILAG"
+jamb_score = 279
+target_year = 2027
+current_year = 2026
+years_left = target_year - current_year
+points_needed = 320 - jamb_score
+
+print(f"Years till UNILAG: {years_left}")
+print(f"Points to gain for 320: {points_needed}")
+print(f"In {years_left} year, {name} becomes a UNILAG student")
