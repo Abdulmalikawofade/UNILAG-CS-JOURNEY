f5cd1bb39c05f13d7d4de3f2ce01bd8103558c23
Abdulmalikawofade <abdulmalikawofade@gmail.com>
2026-05-02 18:26:02 -0700

    Day4: if/elif/else - making decisions like i didn't

---
+++ day4.py
@@ -0,0 +1,16 @@
+# Day 4: Decions like Abdulmalik
+jamb_score = 279
+unilag_cutoff = 200
+dream_score = 320
+
+print("=== Abdulmalik's Admission Checker ===")
+
+if jamb_score >= dream_score:    print("279+ gang! You're HIM FR")
+elif jamb_score >= unilag_cutoff:
+    print("279 cleared UNILAG! your're in")
+    print("No fake friends needed for this W")
+else: print("keep grinding 279 was just warm up")
+
+mood = "healing"
+if mood == "healing": print("Choosing yourself >>> choosing them")
+else: print("We debug that mood next")
